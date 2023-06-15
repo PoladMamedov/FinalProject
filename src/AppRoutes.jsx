a5d60fa435796eeda44bcf679254e5a20ab88dbb
Polad Mamedov <123903403+PoladMamedov@users.noreply.github.com>
2023-06-16 00:56:30 +0300

    Log in page added (#8)
    * basic log in page skeleton
    
    * added align items to flex mixin
    
    * createt basic layout for login page
    
    * added formik and yup validation
    
    * constants reorganization

--- src/AppRoutes.jsx
+++ src/AppRoutes.jsx
@@ -1,15 +1,17 @@
-import { Routes, Route } from 'react-router-dom';
-import Testpages from './pages/TestPages/Testpages';
+import { Routes, Route } from "react-router-dom";
+import LoginPage from "./pages/LoginPage/LoginPage";
+import RegistrationPage from "./pages/RegistrationPage/RegistrationPage";
 
 const AppRoutes = () => {
-    return (
-        <div className='container'>
-          <Routes>
-          <Route path="/" element={<Testpages />} />
-          {/* добавить роуты , Testpages удалить*/}
-        </Routes>
-        </div>
-    );
-}
+  return (
+    <div className="container">
+      <Routes>
+        <Route path="/" element={<h1>mainpage</h1>} />
+        <Route path="/login" element={<LoginPage />} />
+        <Route path="/registration" element={<RegistrationPage />} />
+      </Routes>
+    </div>
+  );
+};
 
 export default AppRoutes;
---
+++ src/pages/RegistrationPage/RegistrationPage.jsx
@@ -0,0 +1,12 @@
+import { Link } from "react-router-dom";
+
+function RegistrationPage() {
+  return (
+    <div>
+      <h1>REGISTRATION</h1>
+      <p style={{ color: "red" }}>NOT ADDED YET</p>
+    </div>
+  );
+}
+
+export default RegistrationPage;
