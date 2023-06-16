8c5efef761ff50c1232777ea8f9b5a96cab1fec3
Egoor_03 <e.zavertalyuk@gmail.com>
2023-06-16 09:10:53 +0300

    add redux for menu

--- src/components/header/index.jsx
+++ src/components/header/index.jsx
@@ -2,33 +2,44 @@
 import { NavLink } from "react-router-dom";
 import './style.scss';
 import { useState } from "react";
+import { useSelector, useDispatch } from "react-redux";
+import { toggleMenu } from "../../redux/actions/burgerMenu";
 const Header = () => {
-   const [isMenuOpen, setIsMenuOpen] = useState(false)
+   const dispatch = useDispatch()
+   const { isMenuOpen } = useSelector((state) => state.isMenuOpen)
    const [activePage, setActivePage] = useState(0)
-   const pages = ['Home', 'Products', 'About']
+   const pages = ['Products', 'About']
 
    const handleBtnClick = () => {
-      setIsMenuOpen(!isMenuOpen)
+      dispatch(toggleMenu())
    }
    const handleLinkClick = (index) => {
       setActivePage(index)
-      setIsMenuOpen(!isMenuOpen)
+      dispatch(toggleMenu())
    }
+
    return (
       <header className="header">
          <div className='header__container'>
             <NavLink
-               className='header__company-logo' to="/">
+               className='header__company-logo' to="/"
+               onClick={() => handleLinkClick(0)}>
                <img src="/img/main-logo.png" alt="" width={150} />
             </NavLink>
 
             <nav className={`header__nav${isMenuOpen ? '--open' : ''}`}>
                <ul className="header__nav-list">
+                  <li className="header__nav-item" key={1}>
+                     <NavLink
+                        className={`header__nav-link${activePage === 0 ? '--active' : ''}`}
+                        onClick={() => handleLinkClick(0)}
+                        to={'/'}>Home</NavLink >
+                  </li>
                   {pages.map((item, index) => (
-                     <li className="header__nav-item" key={index}>
+                     <li className="header__nav-item" key={index + 1}>
                         <NavLink
-                           className={`header__nav-link${activePage === index ? '--active' : ''}`}
-                           onClick={() => handleLinkClick(index)}
+                           className={`header__nav-link${activePage === index + 1 ? '--active' : ''}`}
+                           onClick={() => handleLinkClick(index + 1)}
                            to={`/${item.toLowerCase()}`}>{item}</NavLink >
                      </li>
                   ))}
--- src/redux/store.js
+++ src/redux/store.js
@@ -1,20 +1,21 @@
-import { createStore, combineReducers, applyMiddleware } from  "redux";
+import { createStore, combineReducers, applyMiddleware } from "redux";
 import { composeWithDevTools } from '@redux-devtools/extension';
 import thunk from "redux-thunk";
 import { persistStore, persistReducer } from 'redux-persist'
 import storage from 'redux-persist/lib/storage' // defaults to localStorage for web
-
+import { burgerMenuReducer as burgerMenu } from "./reducers/burgerMenu";
 
 const rootReducer = combineReducers({
-    // products: productsReducer,
+  // products: productsReducer,
+  isMenuOpen: burgerMenu,
 })
 
 const persistConfig = {
-    key: 'root',
-    storage,
-  }
+  key: 'root',
+  storage,
+}
 
 const persistedReducer = persistReducer(persistConfig, rootReducer);
 
-export const store = createStore(persistedReducer, composeWithDevTools(applyMiddleware(thunk)) );
+export const store = createStore(persistedReducer, composeWithDevTools(applyMiddleware(thunk)));
 export const persistor = persistStore(store);
