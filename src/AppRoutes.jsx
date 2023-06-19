0d1165664f3d90428fb268cc5737a55030cbcdc6
TentaJs <tentait2@gmail.com>
2023-06-19 23:10:13 +0300

    add footer files

--- src/AppRoutes.jsx
+++ src/AppRoutes.jsx
@@ -3,6 +3,7 @@ import { Routes, Route } from "react-router-dom";
 import LoginPage from "./pages/LoginPage/LoginPage";
 import RegistrationPage from "./pages/RegistrationPage/RegistrationPage";
 import Header from "./components/header";
+import Footer from './components/footer'
 
 const AppRoutes = () => {
   return (
@@ -15,6 +16,7 @@ const AppRoutes = () => {
           <Route path="/registration" element={<RegistrationPage />} />
         </Routes>
       </div>
+      <Footer/>
     </>
 
   );
