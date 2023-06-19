9e7caeb3ffb674dfc2e5903052313f4cfd8a163e
Egoor_03 <e.zavertalyuk@gmail.com>
2023-06-19 21:30:50 +0300

    fixes

--- src/components/header/index.jsx
+++ src/components/header/index.jsx
@@ -49,31 +49,21 @@ const Header = () => {
                </div>
             </div>
             <div className="header__container-bottom">
-<<<<<<< HEAD
+
 
                <svg className="header__nav-search" xmlns="http://www.w3.org/2000/svg" height="1.1em" viewBox="0 0 512 512" style={{ fill: "#393d45" }}>
-                  <path d="M416 208c0 45.9-14.9 88.3-40 122.7L502.6 457.4c12.5 12.5 12.5 32.8 0 45.3s-32.8 12.5-45.3 0L330.7 
-                 376c-34.4 25.2-76\docs\latest\rules\no-trailing-spaces.8 40-122.7 40C93.1 416 0 322.9 0 208S93.1 0 208 0S416 93.1 416 208zM208 352a144 144 0 1 0 0-288 144 144 0 1 0 0 288z"/>
-=======
-               <svg className="header__nav-search" xmlns="http://www.w3.org/2000/svg" height="1.1em" viewBox="0 0 512 512" style={{ fill: "#393d45" }}>
-                  <path d="M416 208c0 45.9-14.9 88.3-40 122.7L502.6 457.4c12.5 12.5 12.5 32.8 0 45.3s-32.8 12.5-45.3 0L330.7 376c-34.4 25.2-76.8 40-122.7 40C93.1 416 0 322.9 0 208S93.1 0 208 0S416 93.1 416 208zM208 352a144 144 0 1 0 0-288 144 144 0 1 0 0 288z" />
+                  <path d="M416 208c0 45.9-14.9 88.3-40 122.7L502.6 457.4c12.5 12.5 12.5 32.8 0 45.3s-32.8 12.5-45.3 0L330.7  376c-34.4 25.2-76\docs\latest\rules\no-trailing-spaces.8 40-122.7 40C93.1 416 0 322.9 0 208S93.1 0 208 0S416 93.1 416 208zM208 352a144 144 0 1 0 0-288 144 144 0 1 0 0 288z" />
 
->>>>>>> main
                </svg>
                <NavLink
                   className="header__company-logo"
                   to="/"
                   onClick={() => handleLinkClick(0)}>
-<<<<<<< HEAD
+
                   <img src="/img/main-logo.png" alt="" />
                </NavLink>
                <nav className={`header__nav${isMenuOpen ? "--open" : "''"}`}>
-=======
-                  <img src="/img/main-logo.png" alt="main-logo" />
-               </NavLink>
-               <nav className={`header__nav${isMenuOpen ? "--open" : ""}`}>
 
->>>>>>> main
                   <ul className="header__nav-list">
                      <li className="header__nav-item" key={1}>
                         <NavLink
@@ -98,22 +88,12 @@ const Header = () => {
                      key={4}
                      className="header__nav-link--cart"
                   >
-<<<<<<< HEAD
-
-=======
->>>>>>> main
                      <img className="header__nav-cart" src="/img/cart-logo.png" alt="cart-logo" />
                      <span className="header__nav-cart--count">1</span>
 
                      {/* В спан записать с редакса количество в корзине */}
-
-<<<<<<< HEAD
                   </NavLink>
 
-
-=======
-                  </NavLink >
->>>>>>> main
                   <NavLink
                      to={"/login"}
                      key={5}
@@ -123,8 +103,8 @@ const Header = () => {
                      <img className="header__nav-login" src="img/login.png" alt="login-img" />
                   </NavLink>
                </div >
-
                <button
+                  type="button"
                   onClick={() => handleBtnClick()}
                   className={`header__menu-btn${isMenuOpen ? "--active" : ""}`}>
                   <span className="header__menu-lines"></span>
