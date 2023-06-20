7bea29c4eb52bdabc01c14e6f048f23f55c41e46
Polad Mamedov <poladikksp@gmail.com>
2023-06-20 12:35:12 +0300

    added registration page and added features to login page

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,12 @@
 import AppRoutes from "./AppRoutes";
-// import TopItemsSlider from "./components/TopItemsSlider/TopItemsSlider";
+import TopItemsSlider from "./components/TopItemsSlider/TopItemsSlider";
 
 function App() {
   return (
     <>
       <AppRoutes />
       <div className="containerSlider">
-        {/* <TopItemsSlider /> */}
+        <TopItemsSlider />
       </div>
     </>
   );
