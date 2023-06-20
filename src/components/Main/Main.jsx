dea3297e7482cac98eec492819daa88e01104486
Egoor_03 <e.zavertalyuk@gmail.com>
2023-06-20 21:12:50 +0300

    fix

--- src/components/Main/Main.jsx
+++ src/components/Main/Main.jsx
@@ -1,14 +1,15 @@
-import Benefits from "../sections/benefits/BenefitsList";
+
+import Benefits from "../sections/benefits/BenefitsList.jsx";
 import content from "../sections/benefits/content.json";
-import TopItemsSlider from "../TopItemsSlider/TopItemsSlider";
+import TopItemsSlider from "../TopItemsSlider/TopItemsSlider.jsx";
 
 const Main = () => {
     return (
         <main>
-        <div className="containerSlider">
-       <TopItemsSlider />
-      </div>
-          <Benefits content={content}/>
+            <div className="containerSlider">
+                <TopItemsSlider />
+            </div>
+            <Benefits content={content} />
         </main>
     );
 };
