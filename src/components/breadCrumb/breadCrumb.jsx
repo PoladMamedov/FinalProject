4c6b0aed98a8e5c39c4c23affa4b0ceb667c7b27
EgorZave <123360779+EgorZave@users.noreply.github.com>
2023-06-20 13:41:20 +0300

    Bread crumb (#21)

--- src/components/breadCrumb/breadCrumb.jsx
+++ src/components/breadCrumb/breadCrumb.jsx
@@ -50,14 +50,21 @@ const Breadcrumb = () => {
    const handleGoBack = () => {
       history(-1);
    };
+   const handleGoForward = () => {
+      history(+1);
+   };
 
    return (
       <>
          <div className="container">
             <div className="navigation-block">
                {renderBreadcrumb()}
-               <button className="navigation-block__back-btn" type="button" onClick={handleGoBack}>
-               </button>
+               <svg className="navigation-block__back-btn" onClick={handleGoBack} xmlns="http://www.w3.org/2000/svg" height="1.2em" viewBox="0 0 320 512" fill="#393d45">
+                  <path d="M41.4 233.4c-12.5 12.5-12.5 32.8 0 45.3l160 160c12.5 12.5 32.8 12.5 45.3 0s12.5-32.8 0-45.3L109.3 256 246.6 118.6c12.5-12.5 12.5-32.8 0-45.3s-32.8-12.5-45.3 0l-160 160z" />
+               </svg>
+               <svg className="navigation-block__forward-btn" onClick={handleGoForward} xmlns="http://www.w3.org/2000/svg" height="1.2em" viewBox="0 0 320 512" fill="#393d45">
+                  <path d="M278.6 233.4c12.5 12.5 12.5 32.8 0 45.3l-160 160c-12.5 12.5-32.8 12.5-45.3 0s-12.5-32.8 0-45.3L210.7 256 73.4 118.6c-12.5-12.5-12.5-32.8 0-45.3s32.8-12.5 45.3 0l160 160z" />
+               </svg>
             </div>
          </div>
       </>
