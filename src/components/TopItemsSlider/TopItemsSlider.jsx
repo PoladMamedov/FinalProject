2a4063fba3e0e5d97c46533b3c788a5e11b12f2e
Prdrice <prdricen@gmail.com>
2023-06-21 19:14:49 +0200

    doesn[t finish

--- src/components/TopItemsSlider/TopItemsSlider.jsx
+++ src/components/TopItemsSlider/TopItemsSlider.jsx
@@ -10,25 +10,29 @@ const TopItemsSlider = () => {
       id: 1,
       title: "Apple Watch 7X",
       image: "/img/top-slider/watch.jpeg",
-      text: "Embrace the Future of Timekeeping with Apple Watch 7X: Unleash Your Style and Stay Connected in Style."
+      text: "Embrace the Future of Timekeeping with Apple Watch 7X:",
+      subtext: "Unleash Your Style and Stay Connected in Style."
     },
     {
       id: 2,
-      title: "Wireless Headphones Sony G",
+      title: "Headphone Sony G",
       image: "/img/top-slider/headphone04.jpeg",
-      text: "Experience Pure Audio Freedom with Sony G Wireless Headphones: Unleash the Sound."
+      text: "Experience Pure Audio Freedom with Sony G Wireless Headphones:",
+      subtext: "Unleash the Sound."
     },
     {
       id: 3,
       title: "Gaming Keyboard Max",
       image: "/img/top-slider/keyboards-slider.jpeg",
-      text: "Maximize Your Gaming Performance with Gaming Keyboard Max: Unleash Your Competitive Edge."
+      text: "Maximize Your Gaming Performance with Gaming Keyboard Max:",
+      subtext: "Unleash Your Competitive Edge."
     },
     {
       id: 4,
       title: "Optical Mouse G305",
       image: "/img/top-slider/mice02.jpeg",
-      text: "Elevate Your Precision with Optical Mouse G305: Unleash Your Gaming Potential."
+      text: "Elevate Your Precision with Optical Mouse G305:",
+      subtext: "Unleash Your Gaming Potential."
     }
   ];
   const settings = {
@@ -48,7 +52,8 @@ const TopItemsSlider = () => {
            key={item.id} >
             <div className="container">
             <h1 type="button" className="topItems_title">{item.title}</h1>
-            <h3 className="topItems_caption">{item.text}</h3>
+            <h3 className="topItems_text">{item.text}</h3>
+            <h3 className="topItems_subtext">{item.subtext}</h3>
             <a href="/products" className={styles.topItems_link}>
               <button type="button" className="topItems_btn">Shop Now</button></a>
               </div>
