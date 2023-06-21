c6d8902a5960f138bc21ae04986849ee087e8543
EgorZave <123360779+EgorZave@users.noreply.github.com>
2023-06-21 19:00:55 +0300

    Header (#32)
    * create header component
    
    * fix conflict
    
    * create header adaptive
    
    * add redux for menu
    
    * fix bugs
    
    * rework header with new design
    
    * rework with ESLint
    
    * new completed header
    
    * create nav paths under the header
    
    * delete redux state
    
    * add slick dependency
    
    * fixes
    
    * fix main
    
    * fix
    
    * merge main
    
    * add new search input into header
    
    * fixed bugs in header
    
    * fix conflict

--- src/components/main/Main.jsx
+++ src/components/main/Main.jsx
@@ -1,15 +1,16 @@
+
 import Benefits from "../sections/Benefits/BenefitsList";
 import content from "../sections/Benefits/content.json";
-
 import TopItemsSlider from "../TopItemsSlider/TopItemsSlider";
+
 
 const Main = () => {
     return (
         <main>
-        <div className="containerSlider">
-       <TopItemsSlider />
-      </div>
-          <Benefits content={content}/>
+            <div className="containerSlider">
+                <TopItemsSlider />
+            </div>
+            <Benefits content={content} />
         </main>
     );
 };
