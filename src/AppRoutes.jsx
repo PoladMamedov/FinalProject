d4a17bdb1234b88219dd6aa330bb06609c7d1747
Polad Mamedov <poladikksp@gmail.com>
2023-06-22 23:06:38 +0300

    started log reg logic

--- src/AppRoutes.jsx
+++ src/AppRoutes.jsx
@@ -1,25 +1,24 @@
-
 import { Routes, Route } from "react-router-dom";
 import LoginPage from "./pages/LoginPage/LoginPage";
 import RegistrationPage from "./pages/RegistrationPage/RegistrationPage";
 import Header from "./components/header";
 import Home from "./pages/Home/Home";
 import Footer from "./components/footer";
+import UsersCabinet from "./pages/UsersCabinet/UsersCabinet";
 
 const AppRoutes = () => {
   return (
     <>
       <Header />
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/login" element={<LoginPage />} />
-          <Route path="/registration" element={<RegistrationPage />} />
-        </Routes>
-      <Footer/>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/login" element={<LoginPage />} />
+        <Route path="/registration" element={<RegistrationPage />} />
+        <Route path="/cabinet" element={<UsersCabinet />} />
+      </Routes>
+      <Footer />
     </>
-
   );
 };
-
 
 export default AppRoutes;
---
+++ src/components/Notification/Notification.jsx
@@ -0,0 +1,11 @@
+import "./notification.scss";
+
+function Notification() {
+  return (
+    <div className="notification">
+      <p>Wrong password</p>
+    </div>
+  );
+}
+
+export default Notification;
