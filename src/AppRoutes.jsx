cd3c18d842a625ce7a106bf7960612fe61f34658
Natalia Perederia <96496588+Prdrice@users.noreply.github.com>
2023-06-22 17:27:55 +0200

    Top slider (#35)
    * actions
    
    * Change ci.yml
    
    * Change ci.yml
    
    * Add package
    
    * Add package
    
    * fixed bugs for actions
    
    * added new rule:react/destructuring-assigment: off in eslint file
    
    * started doing slider on main page
    
    * slider continuous
    
    * added styles but doesn't finish
    
    * slider doesn't finish but looking better
    
    * doesn[t finish
    
    * slider ready without fetch
    
    * ready styles for slider
    
    * id
    
    * slider ready
    
    ---------
    
    Co-authored-by: Bohdan Admin <test@gmail.com>
    Co-authored-by: Bohdan Admin <bohdanadm1n@gmail.com>

--- src/AppRoutes.jsx
+++ src/AppRoutes.jsx
@@ -2,8 +2,6 @@ import { Routes, Route } from "react-router-dom";
 import LoginPage from "./pages/LoginPage/LoginPage";
 import RegistrationPage from "./pages/RegistrationPage/RegistrationPage";
 import Header from "./components/header";
-import TopItemsSlider from "./components/TopItemsSlider/TopItemsSlider";
-import styles from "./components/TopItemsSlider/TopItemsSlider.scss";
 import Home from "./pages/Home/Home";
 import Footer from "./components/footer";
 import NotFound from "./pages/NotFoundPage/NotFound";
@@ -15,16 +13,6 @@ const AppRoutes = () => {
         <Routes>
           <Route path="*" element={<NotFound />} />
           <Route path="/" element={<Home />} />
-          <Route
-            path="/"
-            element={
-              <>
-                <div className={styles.containerSlider}>
-                  <TopItemsSlider />
-                </div>
-              </>
-            }
-          />
           <Route path="/login" element={<LoginPage />} />
           <Route path="/registration" element={<RegistrationPage />} />
         </Routes>
