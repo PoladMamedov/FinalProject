129be69f23ef6e2ddb9fbf1928e6609c808c04d9
Prdrice <prdricen@gmail.com>
2023-06-23 02:16:32 +0200

    added @media for adaptive site

--- src/components/TopItemsSlider/TopItemsSlider.jsx
+++ src/components/TopItemsSlider/TopItemsSlider.jsx
@@ -28,16 +28,36 @@ const TopItemsSlider = () => {
     speed: 300,
     slidesToShow: 1,
     slidesToScroll: 1,
-    autoplay: true,
-    autoplaySpeed: 6000,
+    // autoplay: true,
+    // autoplaySpeed: 6000,
   };
   return (
+      // <Slider {...settings}>
+      //   {items.map(( item ) => (
+      //     <>
+      //     <div
+      //       className="topItems"
+      //       key={item.id}>
+      //       <div className="container">
+      //         <h1 type="button" className="topItems_title">{item.title}</h1>
+      //         <h3 className="topItems_text">{item.text}</h3>
+      //         <h3 className="topItems_subtext">{item.subtext}</h3>
+      //         <Link to={`/products/${item.customId}`} className={styles.topItems_link}>
+      //           <button type="button" className="topItems_btn">Shop Now</button>
+      //         </Link>
+      //     </div>
+      //     <img
+      //         className="topItems_img"
+      //         src={item.imageUrl}
+      //         alt={item.text} />
+      //     </div>
+      //     </>
+      //   ))}
+      // </Slider>
+      <div className="responsive-slider">
       <Slider {...settings}>
-        {items.map(( item ) => (
-          <>
-          <div
-            className="topItems"
-            key={item.id}>
+        {items.map((item) => (
+          <div className="topItems" key={item.id}>
             <div className="container">
               <h1 type="button" className="topItems_title">{item.title}</h1>
               <h3 className="topItems_text">{item.text}</h3>
@@ -45,15 +65,12 @@ const TopItemsSlider = () => {
               <Link to={`/products/${item.customId}`} className={styles.topItems_link}>
                 <button type="button" className="topItems_btn">Shop Now</button>
               </Link>
+            </div>
+            <img className="topItems_img" src={item.imageUrl} alt={item.text} />
           </div>
-          <img
-              className="topItems_img"
-              src={item.imageUrl}
-              alt={item.text} />
-          </div>
-          </>
         ))}
       </Slider>
+    </div>
   );
 };
 
