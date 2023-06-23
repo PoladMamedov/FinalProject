43359236d08dbb1fa954cbaf7ed122fc9a6b3137
Salamandrachka <104587162+Salamandrachka@users.noreply.github.com>
2023-06-23 00:32:42 -0400

    Categories (#36)
    * categories added
    
    * categories added
    
    * fetch fixed
    
    * categories fixed
    
    ---------
    
    Co-authored-by: Anastasiia Mostovenko <OleksandrivnaSuper@ukr.net>

---
+++ src/components/Categories/index.jsx
@@ -0,0 +1,63 @@
+import React, { useState, useEffect } from "react";
+import { useSelector, useDispatch } from "react-redux";
+import { NavLink } from "react-router-dom";
+import { fetchCategories } from "../../redux/actions/categories";
+import "./style.scss";
+
+function Categories() {
+  const dispatch = useDispatch();
+  const { categories, loading, error } = useSelector(
+    (state) => state.categories
+  );
+  const [activeCategory, setActiveCategory] = useState(null);
+
+  useEffect(() => {
+    dispatch(fetchCategories());
+  }, [dispatch]);
+
+  const groupCategories = (parentId) => {
+    const filteredCategories = categories.filter(
+      (category) => category.parentId === parentId
+    );
+
+    return filteredCategories.map((category) => (
+      // eslint-disable-next-line no-underscore-dangle
+      <li key={category._id} className="subcategory__list-item">
+        <NavLink className="subcategory__list-link">{category.name}</NavLink>
+        {groupCategories(category.id)}
+      </li>
+    ));
+  };
+  if (loading) {
+    return <div>Loading...</div>;
+  }
+  if (error) {
+    console.log(`Error:${error}`);
+  }
+  return (
+    <div className="category">
+      <ul className="category__list">
+        {categories
+          .filter((category) => category.parentId === "null")
+          .map((category) => (
+            <li
+              key={category.id}
+              onMouseEnter={() => setActiveCategory(category.id)}
+              onMouseLeave={() => setActiveCategory(null)}
+              className="category__list-item"
+            >
+              <NavLink className="category__list-link">{category.name}</NavLink>
+
+              {activeCategory === category.id && (
+                <ul className="subcategory__list">
+                  {groupCategories(category.id)}
+                </ul>
+              )}
+            </li>
+          ))}
+      </ul>
+    </div>
+  );
+}
+
+export default Categories;
--- src/redux/store.js
+++ src/redux/store.js
@@ -4,10 +4,12 @@ import thunk from "redux-thunk";
 import { persistStore, persistReducer } from "redux-persist";
 import storage from "redux-persist/lib/storage"; // defaults to localStorage for web
 import pagePathReducer from "./reducers/setPagePath";
+import   categories  from "./reducers/categories";
 
 const rootReducer = combineReducers({
   // products: productsReducer,
-  currentPath: pagePathReducer
+  currentPath: pagePathReducer,
+  categories,
 });
 
 const persistConfig = {
