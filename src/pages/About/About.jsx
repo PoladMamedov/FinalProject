b119b1fbc16774eced0f637cbb03859e0f0e2417
Egoor_03 <e.zavertalyuk@gmail.com>
2023-06-26 21:19:01 +0300

    map marker fix

--- src/pages/About/About.jsx
+++ src/pages/About/About.jsx
@@ -3,24 +3,26 @@ import Map from "../../components/GoogleMap";
 
 const About = () => {
   return (
-    <div className="container">
-      <div className="about__container">
-        <h1 className="about__title">Welcome to Innovation Oasis: Your Ultimate Destination for Electronics and Endless Possibilities!</h1>
-        <div>
-          <img src="" alt="" />
+    <>
+      <div className="container">
+        <div className="about__container">
+          <h1 className="about__title">Welcome to Innovation Oasis: Your Ultimate Destination for Electronics and Endless Possibilities!</h1>
+          <div>
+            <img src="" alt="" />
+          </div>
+          <p>
+            Welcome to Innovation Oasis, your ultimate destination for all things electronics! We are more than just a store – we are a vibrant and energetic team dedicated to bringing you the latest and greatest gadgets with a smile. Get ready to dive into a world of innovation, excitement, and endless possibilities!
+          </p>
+          <p>
+            At Innovation Oasis, we believe that technology should be both functional and fun. That's why we carefully curate our selection of smart watches, headphones, keyboards, and mice to ensure that you not only get top-notch performance but also a touch of personality. Whether you're a tech enthusiast, a fitness junkie, or a music lover, we've got something for everyone.
+          </p>
+          <p>
+            But what truly sets us apart is our team. We are a close-knit group of tech enthusiasts who share a passion for all things electronic. From geeks to gurus, our experts are always up to date with the latest trends and are here to guide you every step of the way. We believe in creating a friendly and welcoming environment where you can ask questions, explore new gadgets, and leave with a smile on your face.
+          </p>
         </div>
-        <p>
-          Welcome to Innovation Oasis, your ultimate destination for all things electronics! We are more than just a store – we are a vibrant and energetic team dedicated to bringing you the latest and greatest gadgets with a smile. Get ready to dive into a world of innovation, excitement, and endless possibilities!
-        </p>
-        <p>
-          At Innovation Oasis, we believe that technology should be both functional and fun. That's why we carefully curate our selection of smart watches, headphones, keyboards, and mice to ensure that you not only get top-notch performance but also a touch of personality. Whether you're a tech enthusiast, a fitness junkie, or a music lover, we've got something for everyone.
-        </p>
-        <p>
-          But what truly sets us apart is our team. We are a close-knit group of tech enthusiasts who share a passion for all things electronic. From geeks to gurus, our experts are always up to date with the latest trends and are here to guide you every step of the way. We believe in creating a friendly and welcoming environment where you can ask questions, explore new gadgets, and leave with a smile on your face.
-        </p>
+        <Map />
       </div>
-      <Map />
-    </div>
+    </>
   );
 };
 
