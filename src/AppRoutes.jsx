4593c7ae7577aafd21bb80583d42b9be62ca4311
TentaJs <tentait2@gmail.com>
2023-06-30 01:46:56 +0300

    add page and add styles

--- src/AppRoutes.jsx
+++ src/AppRoutes.jsx
@@ -9,6 +9,7 @@ import NotFound from "./pages/NotFoundPage/NotFound";
 import RequireAuth from "./components/RequireAuth/RequireAuth";
 import Products from "./pages/Products/Products";
 import About from "./pages/About/About";
+import CartPage from "./pages/CartPage/CartPage";
 
 const AppRoutes = () => {
   return (
@@ -22,6 +23,7 @@ const AppRoutes = () => {
         <Route path="/about" element={<About />} />
         <Route path="/login" element={<LoginPage />} />
         <Route path="/registration" element={<RegistrationPage />} />
+        <Route path="cart/" element={<CartPage />} />
         <Route element={<RequireAuth />}>
           <Route path="/cabinet" element={<UsersCabinet />} />
         </Route>
