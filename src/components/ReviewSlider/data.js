f9bfe288cc85e533b1043c468832e13ea59824b4
Anastasiia Mostovenko <OleksandrivnaSuper@ukr.net>
2023-06-30 00:24:45 -0400

    changes were made

--- src/components/ReviewSlider/data.js
+++ src/components/ReviewSlider/data.js
@@ -4,32 +4,28 @@ const people = [
     image: "/img/review-slider/Person1.jpg",
     name: "Emmy Shults",
     title: "Client",
-    text:
-      "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. ",
+    text: "I ordered headphones last week, and I was amazed at how quickly it arrived. The packaging was secure, ensuring the item was undamaged. The customer service was exceptional, as they kept me updated throughout the entire process. Overall, my experience with Innovation Oasis was outstanding, and I can’t wait for my next purchase.",
   },
   {
     id: 2,
     image: "/img/review-slider/Person2.jpg",
     name: "James Gun",
     title: "Client",
-    text:
-      "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. ",
+    text: "I recently discovered Innovation Oasis while searching for a specific mouse. Not only did they have the item I was looking for, but their selection was vast, and the prices were competitive. The website was easy to navigate, and the product descriptions were informative and accurate. ",
   },
   {
     id: 3,
     image: "/img/review-slider/Person3.jpg",
     name: "Michaela Smith",
     title: "Client",
-    text:
-      "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged.",
+    text: "I recently purchased a keyboard from Innovation Oasis, and I couldn’t be happier with my online shopping experience. Their website was user-friendly, making it easy to find the perfect item. The checkout process was smooth, and I received my order promptly. I’m thrilled with the quality and will definitely shop at Innovation Oasis again in the future. Highly recommended!",
   },
   {
     id: 4,
     image: "/img/review-slider/Person4.jpg",
     name: "James Van",
     title: "Client",
-    text:
-      "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged.",
+    text: "Innovation Oasis is one of the best sites for all your gadgets! I've already bought quite a few things there and I'm not going to stop... The service was impressive and very fast. I recived all my orders without any damages.I really liked the attitude of the working staff. I also want to note that the site is really convenient and easy to navigate.  I will definitely recommend them!",
   },
 ];
 
