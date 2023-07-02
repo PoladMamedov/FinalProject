637e44e0893cf82fd22d059b8f109750db693fac
Chegrynets Angelika <119502326+Lika4y@users.noreply.github.com>
2023-07-02 11:32:43 +0300

    Filter (#64)
    * created mobile version
    
    * add function for clear price inputs
    
    * delete component Main
    
    * add clear checkbox
    
    * add redux to counterfilter
    
    * add filter to categories
    
    * add global state with filtered products and add to hook function
    
    * add fitered products with categories and prices
    
    * add state filterCount and filteredProducts in blacklist local storage, add validation price
    
    * add component SortFilter
    
    * add styles to sortfilter
    
    * filter final
    
    * add opasity to btn disabled

---
+++ src/redux/type/counterFilter.js
@@ -0,0 +1,7 @@
+const counterFilterTypes = {
+    INCREASE_COUNTER: "INCREASE_COUNTER",
+    DECREASE_COUNTER: "DECREASE_COUNTE",
+    RESET_COUNTER: "RESET_COUNTER",
+  };
+  
+  export default counterFilterTypes;
