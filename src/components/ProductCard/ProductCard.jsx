8daddbb7b890b8169d74e44700d348231ffdca0b
Mykola Bezschastnyi <bezschastnyi@gmail.com>
2023-07-05 17:49:39 +0300

    update

--- src/components/ProductCard/ProductCard.jsx
+++ src/components/ProductCard/ProductCard.jsx
@@ -10,15 +10,18 @@ export default function ProductCard(props) {
       <div className={props.active ? "all-card-container" : "card-container"}>
         <div className={props.active ? "all-card" : "card"} style={{backgroundImage: `url(${urlImg})`}} >
           <div className={props.active ? "all-card__btn" : "card__btn"}>
-            <Link className={props.active ? "all-card__btn-details" : "card__btn-details"} to={`/details/${urlItemNumber}`}>DETAIL</Link>
-            <button type={"button"} className={props.active ? "all-card__btn-cartContainer" : "card__btn-cartContainer"}>
+            <Link className={props.active ? "all-card__btn-details" : "card__btn-details"} to={`/products/${urlItemNumber}`}>DETAIL</Link>
+            <button type={"button"} className={props.active ? "all-card__btn-card-container" : "card__btn-card-container"}>
               <img className={props.active ? "all-card__btn-svg-cart" : "card__btn-svg-cart"} src="/img/cart-logo.png" alt="cart-logo" />
             </button>
           </div>
         </div>
         <div className={props.active ? "all-card__block" : "unactive"} >
           <div className={"all-card__product-name"} >{props.item.name}</div>
-          <div className={"all-card__product-price"} >{props.item.currentPrice}</div>
+          {(props.item.previousPrice - props.item.currentPrice !== 0) ? <div className="all-card__prices-wrap">
+            <p className="all-card__price--prev">${props.item.previousPrice}</p>
+            <p className="all-card__price--curr">${props.item.currentPrice}</p>
+          </div> : <div className={"all-card__price--curr"}>${props.item.currentPrice}</div>}
         </div>
       </div>
 
