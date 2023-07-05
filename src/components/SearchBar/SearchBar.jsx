0fb76530d86ebb5fa0671c6511388eb10dbb72f2
Egoor_03 <e.zavertalyuk@gmail.com>
2023-07-05 12:01:10 +0300

    create responsive searched list

--- src/components/SearchBar/SearchBar.jsx
+++ src/components/SearchBar/SearchBar.jsx
@@ -52,6 +52,12 @@ const SearchBar = () => {
     const actualSearchTerm = e.currentTarget.value;
     setSearchTerm(actualSearchTerm);
   };
+  const handleSearchCLose = () => {
+    setIsSearchOpen(false);
+    setSearchTerm("");
+    dispatch(setSearchProducts([]));
+
+  };
 
   return (
     <>
@@ -65,6 +71,13 @@ const SearchBar = () => {
       >
         <path d="M416 208c0 45.9-14.9 88.3-40 122.7L502.6 457.4c12.5 12.5 12.5 32.8 0 45.3s-32.8 12.5-45.3 0L330.7 376c-34.4 25.2-76.8 40-122.7 40C93.1 416 0 322.9 0 208S93.1 0 208 0S416 93.1 416 208zM208 352a144 144 0 1 0 0-288 144 144 0 1 0 0 288z" />
       </svg>
+      <div
+        onClick={() => handleSearchCLose()}
+        onKeyDown={() => handleSearchCLose()}
+        role="button"
+        tabIndex={0}
+        className={`search-wrap${isSearchOpen ? "--active" : ""}`}>
+      </div >
       <form
         onSubmit={(e) => handleSubmit(e)}
         className={`header__search-form${isSearchOpen ? "--active" : ""}`}
@@ -76,6 +89,7 @@ const SearchBar = () => {
           name="searchInput"
           value={searchTerm}
           onChange={(e) => handleSearchChange(e)}
+          onFocus={() => setIsSearchOpen(true)}
         />
         <button
           className={`header__search-submit${isSearchOpen ? "--active" : ""}`}
@@ -109,8 +123,8 @@ const SearchBar = () => {
             );
           })}
         </ul>}
+      </form >
 
-      </form>
     </>
   );
 };
