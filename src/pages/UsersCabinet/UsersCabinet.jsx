c40407b3c377c5d8da0046e2bb555fc77a502036
Polad Mamedov <poladikksp@gmail.com>
2023-07-07 22:49:46 +0300

    starting

--- src/pages/UsersCabinet/UsersCabinet.jsx
+++ src/pages/UsersCabinet/UsersCabinet.jsx
@@ -1,5 +1,5 @@
 import { useEffect } from "react";
-import { useDispatch } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
 import { clearErrorAndMessages, setUserInfo } from "../../redux/actions/user";
 import PersonalDataEditForm from "../../components/PersonalDataEditForm/PersonalDataEditForm";
 import PasswordChangeForm from "../../components/PasswordChangeForm/PasswordChangeForm";
@@ -7,6 +7,8 @@ import Breadcrumb from "../../components/BreadCrumb/BreadCrumb";
 
 function UsersCabinet() {
   const dispatch = useDispatch();
+  // eslint-disable-next-line object-curly-newline
+  const { firstName, lastName, login, email } = useSelector((state) => state.user.userInfo);
   useEffect(() => {
     return () => {
       dispatch(clearErrorAndMessages());
@@ -15,14 +17,15 @@ function UsersCabinet() {
   return (
     <>
       <Breadcrumb />
-
       <section className="users-cabinet">
-        <div className="container">
-          <h2 className="section-title">Personal data</h2>
-          <PersonalDataEditForm />
-          <h2 className="section-title">Password</h2>
-          <PasswordChangeForm />
-          <div className="users-cabinet__logout-btn-wrapper">
+        <div className="container users-cabinet__content-wrapper">
+          <aside className="users-cabinet__info">
+            <img width={200} height={200} src="./img/user-cabinet/profile.png" alt="" />
+            <p>{firstName}</p>
+            <p>{lastName}</p>
+            <p>{login}</p>
+            <p>{email}</p>
+            {/* <div className="users-cabinet__logout-btn-wrapper"> */}
             <button
               className="users-cabinet__btn logout-btn"
               type="button"
@@ -32,6 +35,13 @@ function UsersCabinet() {
             >
               LOG OUT
             </button>
+            {/* </div> */}
+          </aside>
+          <div>
+            <h2 className="section-title">Personal data</h2>
+            <PersonalDataEditForm />
+            <h2 className="section-title">Password</h2>
+            <PasswordChangeForm />
           </div>
         </div>
       </section>
