a9e74a07b69380163c4cb63bab0e559a8a1de471
Lika4y <guralika@gmail.com>
2023-07-10 23:56:22 +0300

    changed hook

--- src/hooks/useServer.js
+++ src/hooks/useServer.js
@@ -113,15 +113,7 @@ export default function useServer() {
       .catch((err) => err);
     return filters;
   }
-  // Get filters categories
-  // async function getFiltersCategories(categories, sort) {
-  //   const filteredProducts = await fetch(`${url}/products/filter?categories=${categories.join(
-  //     ","
-  //   )}&sort=${sort}currentPrice`)
-  //     .then((res) => res.json())
-  //     .catch((err) => err);
-  //   return filteredProducts;
-  // }
+  // Get filters categoriesy
 
   async function getFiltersCategories(categories, sort) {
     const filteredProducts = await fetch(`${url}/products/filter?categories=${categories}&sort=${sort}currentPrice`)
@@ -131,14 +123,7 @@ export default function useServer() {
   }
 
   // Get filters categories+price
-  // async function getFiltersCategoriesPrices(categories, min, max, sort) {
-  //   const filteredProducts = await fetch(`${url}/products/filter?categories=${categories.join(
-  //     ","
-  //   )}&minPrice=${min}&maxPrice=${max}&sort=${sort}currentPrice`)
-  //     .then((res) => res.json())
-  //     .catch((err) => err);
-  //   return filteredProducts;
-  // }
+
   async function getFiltersCategoriesPrices(categories, min, max, sort) {
     const filteredProducts = await fetch(`${url}/products/filter?categories=${categories}&minPrice=${min}&maxPrice=${max}&sort=${sort}currentPrice`)
       .then((res) => res.json())
