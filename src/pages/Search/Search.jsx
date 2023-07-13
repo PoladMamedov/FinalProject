a4d73dba3482666cde6cd3db148f694607cc369a
Egoor_03 <e.zavertalyuk@gmail.com>
2023-07-13 14:07:00 +0300

    fix link styles

--- src/pages/Search/Search.jsx
+++ src/pages/Search/Search.jsx
@@ -1,15 +1,14 @@
-// import { useSelector } from "react-redux";
 import Breadcrumb from "../../components/BreadCrumb/BreadCrumb";
 import AllProductItems from "../../components/AllProductItems/AllProductItems";
 
 const Search = () => {
-   // const { searchResults } = useSelector((state) => state.search);
-   // const isCardView = useSelector((state) => state.toggleCard.cardView);
+
    return (
       <section>
          <div className="container">
             <Breadcrumb />
             <div className="products-section">
+
                <AllProductItems />
             </div>
          </div>
