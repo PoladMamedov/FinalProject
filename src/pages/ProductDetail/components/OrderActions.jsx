2734342ddf4255cedfe821e66891aef76ee80c9e
IraKaleniuk <86426106+IraKaleniuk@users.noreply.github.com>
2023-07-19 12:24:18 +0300

    Product detail page (#110)
    * Added responsive layout &stylized component
    
    * added useParams
    
    * changed mock data on request
    
    * Added img overview (changing main img, carousel of additional imgs)
    
    * Turned oof 2 rules ESlint jsx-a11y
    
    * Added product amount input validation & full screen carousel
    
    * Changed css classes name & disabled scroll when is full creen carousel open
    
    * Fixed component bugs
    
    * Resolved merge conflict
    
    * added specs and color blocks
    
    * Changed carousel arrows
    
    * Changed color block
    
    * Fixed css bugs in price block
    
    * Resolved merge conflict
    
    * Fixed cart block style
    
    * Fixed bugs in tech specifications view
    
    * Split component into parts, fixed styles
    
    * Added logic to dd to cart btn & changeg full screen slider style
    
    * Changed logic in order actions
    
    * Added notifications
    
    * Added cart to redux
    
    * Added the ability to change the currency
    
    * Fixed folder name
    
    * Removed extra files
    
    * Fixed styles
    
    * Fixed style bugs
    
    * Fixed add button bugs when unauthorised
    
    * Added styles when max quantity of product is already in cart
    
    * Changed order actions title attribute
    
    * Changed order actions title attribute
    
    * Added .idea to .gitignore
    
    * Fixed bugs
    
    * Fixed add btn logic
    
    * Fixed styles for iOS
    
    * Fixed styles for iOS
    
    * Resolved merge conflicts
    
    * Remove extra console log

--- src/pages/ProductDetail/components/OrderActions.jsx
+++ src/pages/ProductDetail/components/OrderActions.jsx
@@ -1,5 +1,6 @@
-import { Link } from "react-router-dom";
 import React, { useEffect, useState } from "react";
+import { Link } from "react-router-dom";
+import { Tooltip } from "react-tooltip";
 import { useDispatch, useSelector } from "react-redux";
 import { Store } from "react-notifications-component";
 import useServer from "../../../hooks/useServer";
@@ -61,6 +62,12 @@ export default function OrderActions(props) {
   }, [cart]);
 
 
+  useEffect(() => {
+    const productInCart = cart.find(({product: {_id: id}}) => id === productID);
+    if (productInCart && productInCart.cartQuantity === quantity) setOutOfStock(true);
+  }, [cart]);
+
+
   async function addToFavs() {
     try {
       setIsFav(true);
@@ -86,67 +93,74 @@ export default function OrderActions(props) {
   }
 
   async function onAddButtonClick() {
-      try {
-        const productInCart = cart.find(({product: {_id: id}}) => id === productID);
+    try {
+      if (token) dispatch(fetchCart(token));
 
-        if (token) {
-          dispatch(fetchCart(token));
-          dispatch(setCart({
-            products: [
-              {
-                product: productID,
-                cartQuantity: productInCart ? orderQuantity + productInCart.cartQuantity : orderQuantity
-              }
-            ]
-          }, token));
-        } else {
-          const productInCartIndx = cart.findIndex(({product: {_id: ID}}) => ID === productID);
+      const productInCart = cart.find(({product: {_id: id}}) => id === productID);
+      const productInCartIndx = cart.findIndex(({product: {_id: id}}) => id === productID);
+      const filteredCart = cart.filter((product, index) => index !== productInCartIndx);
 
-          if (productInCart) {
-            const filteredCart = cart.filter((product, index) => index !== productInCartIndx);
-
-            dispatch(fillCart([
-                  ...filteredCart,
-                {
-                  product: {...props},
-                  cartQuantity: orderQuantity + productInCart.cartQuantity
-                }
-              ]));
-          } else {
-            dispatch(addToCart([
-                {
-                  product: {...props},
-                  cartQuantity: productInCart ? orderQuantity + productInCart.cartQuantity : orderQuantity
-                }
-              ]));
+      if (token && filteredCart.length === 0) {
+        dispatch(setCart({
+          products: [
+            {
+              product: productID,
+              cartQuantity: productInCart ? orderQuantity + productInCart.cartQuantity : orderQuantity
             }
+          ]
+        }, token));
+      } else if (token && filteredCart.length) {
+        dispatch(setCart({
+          products: [
+            ...filteredCart.map(({product: {_id: id}, cartQuantity}) => ({product: id, cartQuantity})),
+            {
+              product: productID,
+              cartQuantity: productInCart ? orderQuantity + productInCart.cartQuantity : orderQuantity
+            }
+          ]
+        }, token));
+      } else if (productInCart) {
+        dispatch(fillCart([
+          ...filteredCart,
+          {
+            product: {...props},
+            cartQuantity: orderQuantity + productInCart.cartQuantity
           }
-
-        setOrderQuantity(1);
-
-        Store.addNotification({ ...notificationsSettings.basic, ...notificationsSettings.addedToCart });
-      } catch (error) {
-        Store.addNotification({...notificationsSettings.basic, ...notificationsSettings.error, message: error.message});
+        ]));
+      } else {
+        dispatch(addToCart([
+          {
+            product: {...props},
+            cartQuantity: productInCart ? orderQuantity + productInCart.cartQuantity : orderQuantity
+          }
+        ]));
       }
+
+      setOrderQuantity(1);
+
+      Store.addNotification({ ...notificationsSettings.basic, ...notificationsSettings.addedToCart });
+    } catch (error) {
+      Store.addNotification({...notificationsSettings.basic, ...notificationsSettings.error, message: error.message});
     }
+  }
 
   return <div className="product-detail__order-actions">
     {currentPrice < previousPrice ? <div className="product-detail__price-wrap">{Object.entries(price).map(([key, value], index) => <p
-          key={index}
-          className={`product-detail__price product-detail__price-${key === "currentPrice" ? "current" : "previous"}`}
-      >
-       <CurrencyIcon currency={currencyName} className={`product-detail__currency-icon product-detail__currency-icon-${key === "currentPrice" ? "current" : "previous"}`} color={key === "currentPrice" ? "#f84147" : "#393D45FF"}/>
-        {Math.floor(value * currencyValue)}
-      </p>)}
-    </div>
-      : <p className="product-detail__price">
-         <CurrencyIcon currency={currencyName} className={"product-detail__currency-icon"} color={"#393D45FF"}/>
-        {Math.floor(currentPrice * currencyValue)}
-      </p>}
+            key={index}
+            className={`product-detail__price product-detail__price-${key === "currentPrice" ? "current" : "previous"}`}
+        >
+          <CurrencyIcon currency={currencyName} className={`product-detail__currency-icon product-detail__currency-icon-${key === "currentPrice" ? "current" : "previous"}`} color={key === "currentPrice" ? "#f84147" : "#393D45FF"}/>
+          {Math.floor(value * currencyValue)}
+        </p>)}
+        </div>
+        : <p className="product-detail__price">
+          <CurrencyIcon currency={currencyName} className={"product-detail__currency-icon"} color={"#393D45FF"}/>
+          {Math.floor(currentPrice * currencyValue)}
+        </p>}
     <div className="order-actions__btns-wrap">
       {isFav ? <FavoritesIcon color={"red"} className={"order-actions__favs-btn order-actions__favs-btn--fill"} isFill clickHandler={() => deleteFromFavs()}/>
-        : <FavoritesIcon color={"red"} className={"order-actions__favs-btn"} clickHandler={() => addToFavs()}/>}
-      <button disabled={outOfStock} title={outOfStock ? "Max quantity of product \n is already in cart" : ""} type="button" className="order-actions__add-btn" onClick={onAddButtonClick}>Add to cart</button>
+          : <FavoritesIcon color={"red"} className={"order-actions__favs-btn"} clickHandler={() => addToFavs()}/>}
+      <button disabled={outOfStock} type="button" className="order-actions__add-btn" onClick={onAddButtonClick}><span className={"order-actions__add-btn-inner"} data-tooltip-id={outOfStock ? "order-actions_add-btn" : ""} data-tooltip-content={"Max quantity is already in cart"}>Add to cart</span></button>
     </div>
     <div className="product-detail__color-wrap">
       <p className="product-detail__basic-spec">Color: <span className="product-detail__basic-spec-value">{productColor}</span></p>
@@ -155,5 +169,9 @@ export default function OrderActions(props) {
       </div>
     </div>
     <OrderQuantity productQuantity={quantity} orderQuantity={orderQuantity} setOrderQuantity={setOrderQuantity} productID={productID} cart={cart} outOfStock={outOfStock}/>
+    <Tooltip
+        id="order-actions_add-btn"
+        place="top"
+    />
   </div>;
 }
