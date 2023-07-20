098419448003f38ec988d1a993e1bf23bbc2593a
Prdrice <prdricen@gmail.com>
2023-07-21 01:16:42 +0200

    added button and functions in favorites

--- src/pages/Favorites/Favorites.jsx
+++ src/pages/Favorites/Favorites.jsx
@@ -1,9 +1,9 @@
 import React from "react";
 import { Link } from "react-router-dom";
 import { useSelector, useDispatch } from "react-redux";
-import {
-  removeFromFavorites,
-} from "../../redux/actions/favorites";
+import { removeFromFavorites } from "../../redux/actions/favorites";
+import BreadCrumb from "../../components/BreadCrumb/BreadCrumb";
+import { increaseCart, increaseCartAsync } from "../../redux/actions/cart";
 
 const Favorites = () => {
   const favorites = useSelector((state) => state.favorites.favorites);
@@ -17,7 +17,7 @@ const Favorites = () => {
   const handleRemoveFromFavorites = (product) => {
     dispatch(removeFromFavorites(product));
   };
- 
+
   const onAddItemToCart = async (item, token, productInfo) => {
     try {
       if (token) {
@@ -32,66 +32,65 @@ const Favorites = () => {
 
   return (
     <>
-    <BreadCrumb />
-    <section className="favorites">
-      <div className={"container"}>
-        <div className="favorites__header">
-          <p className="favorites__header-list">Product</p>
-          <p className="favorites__header-list">Price</p>
-          <p className="favorites__header-list">Add to Cart</p>
-          <p className="favorites__header-list">Delete</p>
-        </div>
-        <div className="favorites__item-block">
-          {favorites && favorites.map((product) => (
-            <div className="favorites__item" key={product.itemNo}>
-              {product.imageUrls && product.imageUrls[0] && (
-               <Link className="cart-list__item-image-wrap" to={`/products/${product.itemNo}`}>
-                <img
-                  className={"favorites__item-img"}
-                  src={product.imageUrls[0]}
-                  alt="item-img"
-                />
-                </Link>
-              )}
-              <div className="favorites__item-details">
-                <p className="favorites__item-title">
-                  <Link to={`/products/${product.itemNo}`} className="cart-list__item-title">{product.name}</Link>
-                  </p>
-                <p className="favorites__item-price">
-                  <img
-                    className={"favorites__item-img"}
-                    src={product.imageUrls[0]}
-                    alt="item-img"
-                  />
-                  {Math.floor(product.currentPrice * currencyValue)}
-                </p>
-                <div className="favorites__item-add">
-                  <button
-                   className="favorites__item-add-cart"
+      <BreadCrumb />
+      <section className="favorites">
+        <div className={"container"}>
+          <div className="favorites__header">
+            <p className="favorites__header-list">Product</p>
+            <p className="favorites__header-list">Price</p>
+            <p className="favorites__header-list">Add to Cart</p>
+            <p className="favorites__header-list">Delete</p>
+          </div>
+          <div className="favorites__item-block">
+            {favorites && favorites.map((product) => (
+                <div className="favorites__item" key={product.itemNo}>
+                  {product.imageUrls && product.imageUrls[0] && (
+                    <Link className="cart-list__item-image-wrap" to={`/products/${product.itemNo}`}>
+                 <img
+                   className={"favorites__item-img"}
+                   src={product.imageUrls[0]}
+                   alt="item-img"
+                 />
+                 </Link>
+                  )}
+                  <div className="favorites__item-details">
+                    <p className="favorites__item-title"><Link to={`/products/${product.itemNo}`} className="cart-list__item-title">{product.name}</Link></p>
+                    <p className="favorites__item-price">
+                      <img
+                        className="currency-icon--rows"
+                        src={`/img/currency/${currencyName}-icon.png`}
+                        alt="currency-icon"
+                      />
+                      {Math.floor(product.currentPrice * currencyValue)}
+                    </p>
+                    <div className="favorites__item-add">
+                    <button
+                    className="favorites__item-add-cart"
+                    type="button"
+                    onClick={() => onAddItemToCart(product.itemNo, userToken, product)}>
+                     <img
+                     className={"favorites__item-add-cart-icon"}
+                     src="/img/cart-logo.png"
+                     alt="delete item from favorites"
+                   />
+                   </button>
+                    </div>
+                    <div className="favorites__item-remove">
+                 <button
+                   className={"favorites__item-remove-btn"}
+                   onClick={() => handleRemoveFromFavorites(product.itemNo)}
                    type="button"
-                   onClick={() => onAddItemToCart(product.itemNo, userToken, product)}>
-                    <img
-                    className={"favorites__item-add-cart-icon"}
-                    src="/img/cart-logo.png"
-                    alt="delete item from favorites"
-                  />
-                  </button>
+                 >
+                   <img
+                     className={"favorites__item-remove-icon"}
+                     src="/img/cart-trash-icon.png"
+                     alt="delete item from favorites"
+                   />
+                 </button>
+               </div>
+                  </div>
                 </div>
-                <div className="favorites__item-remove">
-                <button
-                  className={"favorites__item-remove-btn"}
-                  onClick={() => handleRemoveFromFavorites(product.itemNo)}
-                  type="button"
-                >
-                  <img
-                    className={"favorites__item-remove-icon"}
-                    src="/img/cart-trash-icon.png"
-                    alt="delete item from favorites"
-                  />
-                </button>
-              </div>
-              </div>
-            ))}
+              ))}
             <div className="favorites__button">
               <Link to={"/"} className={"favorites__button-close"}>
                 Close
@@ -99,8 +98,7 @@ const Favorites = () => {
             </div>
           </div>
         </div>
-      </div>
-    </section>
+      </section>
     </>
   );
 };
