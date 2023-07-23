9cf803e4c08be95b5d8ef935dde45d9113f2bed1
martirosyan5 <118726469+martirosyan5@users.noreply.github.com>
2023-07-23 12:52:23 +0200

    Cloudinary (#128)
    * not completed yet
    
    * for almost done
    
    * little change
    
    * c
    
    * c
    
    * fix lock.json
    
    * done
    
    * some changes
    
    * c
    
    * finished
    
    * completed

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -10,10 +10,10 @@ const Footer = () => {
           <p className="footer__subtitle">©Copyright 2023. Created by OurTeam</p>
         </div>
         <div className="footer__social-icons">
-          <a href="facebook.com" className="social-icons__item social-icons__item--facebook"> <Image cloudName={cloudinaryConfig.cloudName} publicId="footer-facebook-logo_hqjcwr" alt="facebook-link" width={30} /> </a>
-          <a href="twitter.com" className="social-icons__item social-icons__item--twitter"><Image cloudName={cloudinaryConfig.cloudName} publicId="footer-twitter-logo_xxsnnw" alt="twitter-link" width={30} /></a>
+          <a href="facebook.com" className="social-icons__item social-icons__item--facebook"> <Image cloudName={cloudinaryConfig.cloudName} publicId="https://res.cloudinary.com/dfinki0p4/image/upload/v1689177287/footer-icons/footer-facebook-logo_hqjcwr.png" alt="facebook-link" width={30} /> </a>
+          <a href="twitter.com" className="social-icons__item social-icons__item--twitter"><Image cloudName={cloudinaryConfig.cloudName} publicId="https://res.cloudinary.com/dfinki0p4/image/upload/v1689177286/footer-icons/footer-twitter-logo_xxsnnw.png" alt="twitter-link" width={30} /></a>
           <a href="instagram.com" className="social-icons__item social-icons__item--instagram"><Image cloudName={cloudinaryConfig.cloudName} publicId="https://res.cloudinary.com/dfinki0p4/image/upload/v1689604281/footer-icons/footer-instagram-logo_kwlsmr.png" alt="instagram-link" width={30} /></a>
-          <a href="youtube.com" className="social-icons__item social-icons__item--youtube"><Image cloudName={cloudinaryConfig.cloudName} publicId="footer-youtube-logo_lgsgph" alt="youtube-link" width={30} /></a>
+          <a href="youtube.com" className="social-icons__item social-icons__item--youtube"><Image cloudName={cloudinaryConfig.cloudName} publicId="https://res.cloudinary.com/dfinki0p4/image/upload/v1689177286/footer-icons/footer-youtube-logo_lgsgph.png" alt="youtube-link" width={30} /></a>
         </div>
       </div>
     </footer>
