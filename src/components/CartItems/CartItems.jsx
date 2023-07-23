3984d9a38193fefb3c90abd8bf62ff9daacf9da8
Maxim <127696109+TentaJs@users.noreply.github.com>
2023-07-23 19:28:07 +0300

    Cart input (#132)

--- src/components/CartItems/CartItems.jsx
+++ src/components/CartItems/CartItems.jsx
@@ -2,6 +2,7 @@ import { useDispatch, useSelector } from "react-redux";
 import { Link, useLocation } from "react-router-dom";
 import React, { useEffect, useState } from "react";
 import { Image } from "cloudinary-react";
+import { Store } from "react-notifications-component";
 import cloudinaryConfig from "../../config/cloudinaryConfig";
 import {
   removeCartAsync,
@@ -9,38 +10,54 @@ import {
   increaseCart,
   increaseCartAsync,
   decreaseCartAsync,
-  decreaseCart,
+  decreaseCart, updateCartQuantity, setCart
 } from "../../redux/actions/cart";
+import notificationsSettings from "../../constants/constants";
 
 const CartItems = (props) => {
-  const location = useLocation();
-  const isCheckoutPage = location.pathname === "/checkout";
-  // eslint-disable-next-line no-unused-vars
-  const [isCheckout, setIsCheckout] = useState(isCheckoutPage);
-  useEffect(() => {
-    setIsCheckout(isCheckoutPage);
-  }, [isCheckoutPage]);
-
-  const dispatch = useDispatch();
-  const userToken = useSelector((state) => state.user.userInfo.token);
-
   const {
     cartQuantity,
     product: {
       name, currentPrice, imageUrls, itemNo
     },
   } = props.dataProducts;
+  const location = useLocation();
+  const isCheckoutPage = location.pathname === "/checkout";
+  // eslint-disable-next-line no-unused-vars
+  const [isCheckout, setIsCheckout] = useState(isCheckoutPage);
+  const [inputValue, setInputValue] = useState(cartQuantity);
+  const dispatch = useDispatch();
+  const userToken = useSelector((state) => state.user.userInfo.token);
+  const cartProducts = useSelector((state) => state.cart.cart);
   // eslint-disable-next-line no-underscore-dangle
   const itemId = props.dataProducts.product._id;
+  // const cartProducts = useSelector((state) => state.cart.cart);
+  useEffect(() => {
+    setIsCheckout(isCheckoutPage);
+  }, [isCheckoutPage]);
+
+  useEffect(() => {
+    const updatedCart = {
+      products: cartProducts.map((item) => ({
+        // eslint-disable-next-line no-underscore-dangle
+        product: item.product._id,
+        cartQuantity: item.cartQuantity
+      }))
+    };
+    dispatch(setCart(updatedCart, userToken));
+  }, [cartQuantity]);
 
   const OnDeleteItem = async (item, token) => {
     try {
       if (token) {
         dispatch(removeCartAsync(item, token));
+        Store.addNotification({ ...notificationsSettings.basic, ...notificationsSettings.cartDeleted });
       } else {
         dispatch(removeCart(item));
+        Store.addNotification({ ...notificationsSettings.basic, ...notificationsSettings.cartDeleted });
       }
     } catch (error) {
+      Store.addNotification({ ...notificationsSettings.basic, ...notificationsSettings.cartNotDeleted });
       console.log(error);
     }
   };
@@ -49,10 +66,15 @@ const CartItems = (props) => {
     try {
       if (token) {
         dispatch(increaseCartAsync(item, token));
+        setInputValue((prevState) => +prevState + 1);
+        Store.addNotification({ ...notificationsSettings.basic, ...notificationsSettings.cartIncreased });
       } else {
         dispatch(increaseCart(item));
+        setInputValue((prevState) => +prevState + 1);
+        Store.addNotification({ ...notificationsSettings.basic, ...notificationsSettings.cartIncreased });
       }
     } catch (error) {
+      Store.addNotification({ ...notificationsSettings.basic, ...notificationsSettings.cartNotIncreased });
       console.log(error);
     }
   };
@@ -60,15 +82,44 @@ const CartItems = (props) => {
     try {
       if (token) {
         dispatch(decreaseCartAsync(item, token));
+        setInputValue((prevState) => prevState - 1);
+        Store.addNotification({ ...notificationsSettings.basic, ...notificationsSettings.cartDecreased });
       } else {
         dispatch(decreaseCart(item));
+        setInputValue((prevState) => prevState - 1);
+        Store.addNotification({ ...notificationsSettings.basic, ...notificationsSettings.cartDecreased });
       }
     } catch (error) {
       console.log(error);
+      Store.addNotification({ ...notificationsSettings.basic, ...notificationsSettings.cartNotDecreased });
     }
   };
 
-  return (
+  const handleInputChange = (value) => {
+    if (/^\d{0,2}$/.test(value) && !(value.length > 1 && value === "0")) {
+      setInputValue(value);
+    }
+  };
+
+  const handleInputBlur = async (item, value) => {
+    const {quantity} = props.dataProducts.product;
+    const isValidValue = +value !== 0 && +value <= quantity;
+
+    if (isValidValue) {
+      if (userToken) {
+        dispatch(updateCartQuantity(itemId, +value));
+        Store.addNotification({ ...notificationsSettings.basic, ...notificationsSettings.cartQuantityChanged });
+      } else {
+        dispatch(updateCartQuantity(itemId, +value));
+        Store.addNotification({ ...notificationsSettings.basic, ...notificationsSettings.cartQuantityChanged });
+      }
+    } else {
+      Store.addNotification({ ...notificationsSettings.basic, ...notificationsSettings.cartQuantityNotChanged });
+      setInputValue(cartQuantity);
+    }
+  };
+
+    return (
     <>
       <li className={`cart-list__item ${isCheckout ? "none" : ""}`}>
         {/* <img className={"cart-list__item-image"} src={imageUrls[0]} alt="item-img" /> */}
@@ -97,7 +148,9 @@ const CartItems = (props) => {
             <input
               type={"text"}
               className="cart-list__item-quantity-number"
-              value={cartQuantity}
+              value={inputValue}
+              onChange={(event) => handleInputChange(event.target.value)}
+              onBlur={() => handleInputBlur(itemId, inputValue)}
             />
             <button
               type={"button"}
@@ -156,7 +209,7 @@ const CartItems = (props) => {
             <input
               type={"text"}
               className="checkout-cart-list__item-quantity-number"
-              value={cartQuantity}
+              onChange={(event) => handleInputChange(event)}
             />
             <button
               type={"button"}
