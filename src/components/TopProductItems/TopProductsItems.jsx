1ee45a2c8f516a2a15ecf22690ef2570f3bc1907
Mykola Bezschastnyi <bezschastnyi@gmail.com>
2023-07-24 23:23:01 +0300

    fix

--- src/components/TopProductItems/TopProductsItems.jsx
+++ src/components/TopProductItems/TopProductsItems.jsx
@@ -34,13 +34,13 @@ function TopProductItem() {
             <ProductCard item={e} key={e._id} />
           ))}
         </div>
-        <Link className={"top-product__btn-allProduct"} to="/products">
-          Alll Products
+        <div className={"top-product__btn-allProduct"} to="/products">
+          All Products
           <svg className={"top-product__svg"} width="20" height="20" viewBox="0 0 20 20" fill="none" xmlns="http://www.w3.org/2000/svg">
             <path d="M4.16666 10H15.8333" stroke="white" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round"/>
             <path d="M10 4.16667L15.8333 10L10 15.8333" stroke="white" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round"/>
           </svg>
-        </Link>
+        </div>
       </div>
     </section>
   );
