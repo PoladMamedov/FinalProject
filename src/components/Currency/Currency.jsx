5899c73cca7ee807bc6bbcedf68c0ec41def1b48
EgorZave <123360779+EgorZave@users.noreply.github.com>
2023-07-25 18:49:31 +0300

    Final review (#147)
    * create header component
    
    * fix conflict
    
    * create header adaptive
    
    * add redux for menu
    
    * fix bugs
    
    * rework header with new design
    
    * rework with ESLint
    
    * new completed header
    
    * create nav paths under the header
    
    * delete redux state
    
    * add slick dependency
    
    * fixes
    
    * fix main
    
    * fix
    
    * merge main
    
    * final rewiev

--- src/components/Currency/Currency.jsx
+++ src/components/Currency/Currency.jsx
@@ -12,7 +12,7 @@ const Currency = () => {
    const values = ["USD", "UAH", "EUR", "PLN"];
 
    useEffect(() => {
-      fetch("https://openexchangerates.org/api/latest.json?app_id=5fb46af546ac47e288a866419999ae33")
+      fetch("https://openexchangerates.org/api/latest.json?app_id=0a4d9c40af42460c9346c798e2a20698")
          .then((response) => response.json())
          .then((data) => setAllCurrencies(data));
    }, [value]);
