1d6794e2e8bf81ee23f910d96db5f6f228884bbd
Prdrice <prdricen@gmail.com>
2023-07-26 20:22:09 +0200

    nothing

--- src/redux/actions/favorites.js
+++ src/redux/actions/favorites.js
@@ -1,13 +1,122 @@
-export const addToFavorites = (product) => {
+import { Store } from "react-notifications-component";
+import favoritesTypes from "../type/favorites";
+import useServer from "../../hooks/useServer";
+import notificationsSettings from "../../constants/constants";
+
+export const addToFavorites = (products) => {
   return {
-    type: "ADD_TO_FAVORITES",
-    payload: product,
+    type: favoritesTypes.ADD_TO_FAV,
+    payload: { products },
   };
 };
 
 export const removeFromFavorites = (itemId) => {
   return {
-    type: "REMOVE_FROM_FAVORITES",
+    type: favoritesTypes.REMOVE_FROM_FAV,
     payload: itemId,
   };
 };
+
+export function fillFav(products) {
+  return {
+    type: favoritesTypes.FILL_FAV,
+    payload: { products },
+  };
+}
+
+export const fetchFav = (token) => {
+  return async (dispatch) => {
+    const { getFavorites } = useServer();
+    try {
+      const fav = await getFavorites(token);
+      dispatch(fillFav(fav.products));
+    } catch (error) {
+      Store.addNotification({ ...notificationsSettings.basic, ...notificationsSettings.error, message: error.message });
+    }
+  };
+};
+
+export const setFav = (products, token) => {
+  return async (dispatch) => {
+    const { updateFavorites } = useServer();
+    try {
+      const updateFav = await updateFavorites(products, token);
+      dispatch(fillFav(updateFav.products));
+    } catch (error) {
+      Store.addNotification({ ...notificationsSettings.basic, ...notificationsSettings.error, message: error.message });
+    }
+  };
+};
+
+export const increaseFav = (itemId, productInfo) => {
+  return {
+    type: favoritesTypes.INCREASE_FAV,
+    payload: {
+      id: itemId,
+      product: productInfo,
+    },
+  };
+};
+
+export const increaseFavAsync = (itemId, token, productInfo) => {
+  return async (dispatch) => {
+    const { addItemFav } = useServer();
+    try {
+      const addedFav = await addItemFav(itemId, token);
+      dispatch(increaseFav(itemId, productInfo));
+    } catch (error) {
+      Store.addNotification({ ...notificationsSettings.basic, ...notificationsSettings.error, message: error.message });
+    }
+  };
+};
+
+export const decreaseFav = (itemId) => {
+  return {
+    type: favoritesTypes.DECREASE_FAV,
+    payload: {
+      id: itemId,
+    },
+  };
+};
+
+export const decreaseFavAsync = (itemId, token) => {
+  return async (dispatch) => {
+    const { decreaseProductQuantity } = useServer();
+    try {
+      const decreasedFav = await decreaseProductQuantity(itemId, token);
+      dispatch(decreaseFav(itemId));
+    } catch (error) {
+      Store.addNotification({ ...notificationsSettings.basic, ...notificationsSettings.error, message: error.message });
+    }
+  };
+};
+
+export const updateFavQuantity = (itemId, cartQuantity) => {
+  return {
+    type: favoritesTypes.UPDATE_FAV_QUANTITY,
+    payload: {
+      id: itemId,
+      cartQuantity
+    }
+  };
+};
+
+export const removeEntireFav = () => {
+  return {
+    type: favoritesTypes.REMOVE_ENTIRE_FAV,
+  };
+};
+
+// export const addToFavorites = (product) => {
+//   return {
+//     type: "ADD_TO_FAVORITES",
+//     payload: product,
+//   };
+// };
+
+// export const removeFromFavorites = (itemId) => {
+//   return {
+//     type: "REMOVE_FROM_FAVORITES",
+//     payload: itemId,
+//   };
+// };
