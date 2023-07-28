fadfc06ceaf4acde5a0d3ab795940f2b9afa552e
Iryna Kaleniuk <iryna2001ka@gmail.com>
2023-07-28 22:01:35 +0300

    Remove extra notifications

--- src/constants/constants.js
+++ src/constants/constants.js
@@ -76,21 +76,6 @@ const notificationsSettings = {
     message: "Product didn't deleted",
     type: "danger"
   },
-  commentAdded: {
-    title: "Success!",
-    message: "Your comment added",
-    type: "success",
-  },
-  commentDeleted: {
-    title: "Success!",
-    message: "Your comment deleted",
-    type: "success",
-  },
-  commentEdited: {
-    title: "Success!",
-    message: "Your comment edited",
-    type: "success",
-  }
 };
 
 export default notificationsSettings;
