4cdc0a750188a0f5669ee87439a2cfc74a81aed8
Anastasiia Mostovenko <OleksandrivnaSuper@ukr.net>
2023-07-28 12:16:53 -0400

    Confetti added

--- src/pages/CheckOut/components/DeliveryForm/DeliveryForm.jsx
+++ src/pages/CheckOut/components/DeliveryForm/DeliveryForm.jsx
@@ -1,9 +1,6 @@
-// import { useState } from "react";
 import NovaPoshtaForm from "../NovaPoshtaForm/NovaPoshtaForm";
 import StorePickUpForm from "../StorePickUpForm/StorePickUpForm";
 
-// function DeliveryForm() {
-  // const [isDelivery, setIsDelivery] = useState(true);
 function DeliveryForm({ isNovaPoshtaDelivery, setIsNovaPoshtaDelivery }) {
 
   return (
@@ -12,33 +9,27 @@ function DeliveryForm({ isNovaPoshtaDelivery, setIsNovaPoshtaDelivery }) {
       <div className="checkout-section__form-select">
         <button
           className={
-            // isDelivery
             isNovaPoshtaDelivery
               ? "checkout-section__form-select-btn checkout-section__form-select-btn--active"
               : "checkout-section__form-select-btn"
           }
           type="button"
-          // onClick={() => setIsDelivery(true)}
           onClick={() => setIsNovaPoshtaDelivery(true)}
         >
           Delivery
         </button>
         <button
           className={
-            // isDelivery
             isNovaPoshtaDelivery
               ? "checkout-section__form-select-btn"
               : "checkout-section__form-select-btn checkout-section__form-select-btn--active"
           }
           type="button"
-          // onClick={() => setIsDelivery(false)}
           onClick={() => setIsNovaPoshtaDelivery(false)}
         >
           Store Pickup
         </button>
       </div>
-      {/* {isDelivery ? <NovaPoshtaForm /> : <StorePickUpForm />} */}
-
       {isNovaPoshtaDelivery ? <NovaPoshtaForm /> : <StorePickUpForm />}
     </div>
   );
