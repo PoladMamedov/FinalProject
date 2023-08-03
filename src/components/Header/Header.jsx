c979a38cf29eefe0cc3c4233af5462ecd4f36444
Maxim <127696109+TentaJs@users.noreply.github.com>
2023-08-03 22:06:29 +0300

    Add cart functionality (#167)
    * removed unnecessary notifications
    
    * markup fix
    
    * add delete whole cart funcionality
    
    * edit header test

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -74,7 +74,7 @@ const Header = () => {
         <div className="container">
           <div className="header__container-top">
             <h2 className="header__container-top-title">
-              Free shipping across Ukraine
+              Shipping only in Ukraine
             </h2>
             <NavLink
               className="header__company-logo"
